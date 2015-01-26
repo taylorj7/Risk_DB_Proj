3a6219934df6546f6207fb2c00e68a48066a95f6
taylorj7 <taylorj7@rose-hulman.edu>
2015-01-26 16:52:59 -0500

    I added a dummy user... no not an actual dummy just a user with no rights.

---
+++ Database_Instantiation/UserRights.sql
@@ -0,0 +1,17 @@
+USE Risk42
+GO
+GRANT EXECUTE ON [Change Password]
+TO [333Winter2014Risk]
+GO
+GRANT EXECUTE ON [Create Game]
+TO [333Winter2014Risk]
+GO
+GRANT EXECUTE ON [CREATE USER]
+TO [333Winter2014Risk]
+GO
+GRANT EXECUTE ON [Get Active Games]
+TO [333Winter2014Risk]
+GO
+GRANT EXECUTE ON [login]
+TO [333Winter2014Risk]
+GO
