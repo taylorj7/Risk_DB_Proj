7a6a0073673cbb5d7f99a9e93a1bb018b495892b
jaminboy13 <mayja1@rose-hulman.edu>
2015-01-29 16:15:52 -0500

    Added County and Continent queries

---
+++ Database_Instantiation/InsertContinentsQuery.sql
@@ -0,0 +1,20 @@
+Use[Risk42]
+Go
+Insert Into Continents(Name, Bonus_Value)
+Values('Asia', 7)
+Go
+Insert Into Continents(Name, Bonus_Value)
+Values('North America', 5);
+Go
+Insert Into Continents(Name, Bonus_Value)
+Values('South America', 2);
+Go
+Insert Into Continents(Name, Bonus_Value)
+Values('Europe', 5);
+Go
+Insert Into Continents(Name, Bonus_Value)
+Values('Africa', 3);
+Go
+Insert Into Continents(Name, Bonus_Value)
+Values('Australia', 2);
+Go
